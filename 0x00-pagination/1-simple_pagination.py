dfd79c51fc52259848ee840ee39682232fbf70fd
Zainab Salih <Zainab.Salih@outlook.com>
2024-09-02 23:21:47 +0400

    Task 1

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -37,7 +37,13 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            """
-            Return the appropriate page of the dataset
-            """
-            pass
+        """
+        Return the appropriate page of the dataset
+        """
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+
+        dataset = self.dataset()
+        start_index, end_index = index_range(page, page_size)
+
+        return dataset[start_index:end_index]
