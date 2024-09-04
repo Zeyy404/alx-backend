722d6f76e5ce70be784607b2ba524470f1809e20
Zainab Salih <Zainab.Salih@outlook.com>
2024-09-04 20:19:44 +0400

    Tasks 0 - 2

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""class BasicCache module"""
+
+
+BaseCaching = __import__('BaseCaching').base_caching.py
+
+
+class BasicCache(BaseCaching):
+    """Inherits from BaseCaching and is a caching system"""
+    def put(self, key, item):
+        """
+        Assigns to the dictionary `self.cache_data`
+        the `item` value for the key `key`.
+        """
+        if key is None or item is None:
+            return
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Returns the value in `self.cache_data` linked to `key`."""
+        return self.cache_data.get(key, None)
---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""class LIFOCache module"""
+from collections import OrderedDict
+
+
+BaseCaching = __import__('BaseCaching').base_caching.py
+
+
+class LIFOCache(BaseCaching):
+    """Inherits from BaseCaching and is a caching system"""
+    def __init__(self):
+        """initailize the class"""
+        super().__init__()
+        self.cache_data = OrderedDict
+
+    def put(self, key, item):
+        """
+        Assigns to the dictionary self.cache_data
+        the `item` value for the key `key`.
+        """
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            last_key, _ = self.cache_data.popitem(True)
+            print(f"DISCARD: {last_key}")
+
+        self.cache_data[key] = item
+        self.cache_data.move_to_end(key, last=True)
+
+    def get(self, key):
+        """Returns the value in `self.cache_data` linked to `key`."""
+        return self.cache_data.get(key, None)
